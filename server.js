a6975bd1a5dac75566ca244dd9d547f5545fe767
bestinepayyappilly <bestine1234@gmail.com>
2023-09-26 17:43:13 +0530

    some changes

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ const generateWKPDF = (item) => {
     // pageWidth: 176,
     pageWidth: 242,
     // pageHeight: 318,
-    pageHeight: 190,
+    pageHeight: 180,
   });
   //This is a new comment
   return data;
