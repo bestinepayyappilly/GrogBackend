91a22e340dd3c6c8b2057cb858caf105021c29c2
bestinepayyappilly <bestine1234@gmail.com>
2023-09-27 15:00:22 +0530

    uploaded new certs

--- server.js
+++ server.js
@@ -221,8 +221,8 @@ app.post("/api/upload-html", async (req, res) => {
 https
   .createServer(
     {
-      key: fs.readFileSync("./certs/server.key"),
-      cert: fs.readFileSync("./certs/server.cert"),
+      key: fs.readFileSync(__dirname, "/server.key", "utf8"),
+      cert: fs.readFileSync(__dirname, "/server.cert", "utf8"),
     },
     app
   )
